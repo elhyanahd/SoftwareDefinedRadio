78f0c235d217a7dbd58d2aaa8d8b16a6097e4aa4
elhyanahd <elhyanahd@vt.edu>
2025-06-16 08:28:33 -0400

    Updated to incorporate Lab3 specifications. Still need to fix the reading of the files.

--- vitis/hello_world/src/helloworld.c
+++ vitis/hello_world/src/helloworld.c
@@ -19,6 +19,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <ctype.h>
 #include <xil_types.h>
 #include "platform.h"
 #include "xuartps_hw.h"        // R byte from Ps7 UART
@@ -29,6 +30,9 @@
 #include "xiic_l.h"         // R/W I2C (IIC interface)
 #include "sleep.h"
 
+
+static u32 sampleData [4096];
+static u32 sampleSize;
 
 /*
  * @brief: Helper function used to write specific
@@ -46,10 +50,10 @@ void writeCodecReg(u8 regnum, u16 regval)
     {   printf("\r\nwriteCodecReg(%u,%u) failed\r\n", regnum, regval);  }
 }
 
-// /*
-//  * @brief: Function that is used to perform 
-//  *         initial configuration of CODEC.
-//  */
+/*
+ * @brief: Function that is used to perform 
+ *         initial configuration of CODEC.
+ */
 void configureCodec()
 {
     writeCodecReg(15,0x00);
@@ -67,8 +71,6 @@ void configureCodec()
     writeCodecReg(6,0x27);
     usleep(75000);
     writeCodecReg(9,0x01);
-    print("CODEC configured now, press +/- to increase/decrease volume\n\r");
-    print("Remember to include shift for +\n\r");
 }
 
 /*
@@ -83,32 +85,8 @@ void setVolume(u16 volume)
 }
 
 /*
- * @brief: Write given volume to CODEC
- * @param: volume 
- */
-void modifyVolume(u16 *volume)
-{
-    if(XUartPs_IsReceiveData(XPAR_XUARTPS_0_BASEADDR))  
-    {
-        char c = XUartPs_RecvByte(XPAR_XUARTPS_0_BASEADDR);
-    
-        if (c == '+' && *volume < 0x7F) 
-        {   
-            (*volume)++;   
-            setVolume(*volume);
-        } 
-        else if (c == '-' && *volume > 0x00) 
-        {   
-            (*volume)--;  
-            setVolume(*volume); 
-        }
-    }
-}
-
-/*
  * @brief: Cycle through 6.103 kHz sample points
- *         and return that sample point based
- *         on the counter value.
+ *         and send data to AXI FIFO. 
  */
 void waveGenerator()
 {
@@ -123,24 +101,89 @@ void waveGenerator()
     do  {   vacancy = XLlFifo_ReadReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TDFV_OFFSET); }
     while (vacancy < sizeof(word));
 
+    //write word and word size to FIFO
     XLlFifo_WriteReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TDFD_OFFSET, word);
     XLlFifo_WriteReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TLF_OFFSET, sizeof(word));
     counter = (counter + 1) % 8;  // Ensures cycling without if statements
 }
 
+
 /*
  * @brief: 
  */
-void writeToFIFO()
+void writeToFIFO(int counter)
 {
     u32 vacancy = 0x0;
+    int32_t word = sampleData[counter];
     
     //block until FIFO is free
     do  {   vacancy = XLlFifo_ReadReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TDFV_OFFSET); }
-    while (vacancy < sizeof(0x4010));
+    while (vacancy < sizeof(word));
 
-    XLlFifo_WriteReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TDFD_OFFSET, 0x4010);
-    XLlFifo_WriteReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TLF_OFFSET, sizeof(0x4010));
+    XLlFifo_WriteReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TDFD_OFFSET, word);
+    XLlFifo_WriteReg(XPAR_AXI_FIFO_MM_S_0_BASEADDR, XLLF_TLF_OFFSET, sizeof(word));
+}
+
+/*
+ * @brief: Loops through loaded file and stores 4 bytes
+ *         in little edian format (first value LSB). Returns
+ *         32-bit value. 
+ */
+u32 receiveFileData() 
+{
+    u32 data = 0;
+
+    for(int i = 0; i < 4; i++)
+    {
+        while (!XUartPs_IsReceiveData(XPAR_XUARTPS_0_BASEADDR)) {}  // Wait until a byte is received
+
+        if (XUartPs_IsReceiveData(XPAR_XUARTPS_0_BASEADDR)) 
+        {   
+            u8 byte = XUartPs_RecvByte(XPAR_XUARTPS_0_BASEADDR);   
+            data = (byte << 24) | (data >> 8); 
+        }
+    }
+
+    return data;
+}
+
+/*
+ * @brief: Once 'L' is pressed, loop until all file 
+ *         samples are received based on the first 
+ *         32-bit number read.
+ */
+void loadFile()
+{
+    int loaded = 0;
+    u32 size = 0;
+
+    int i = 0;
+    while (loaded == 0)
+    {
+        //retrieve 32 bit word
+        u32 byte = receiveFileData();   
+
+        //If the word is the first 32-bit value
+        //then store as size, else add to global 
+        //array until sample size reached.
+        if (size == 0)
+        {   
+            size = byte;
+            continue;    
+        }
+        else 
+        {
+            sampleData[i] = byte;
+            i++;
+        }
+
+        //End loop once sample size reached.
+        if((u32)i == size)
+        {   loaded = 1; }
+    }
+
+    sampleSize = size;
+    printf("Successfully loaded %d audio samples!\n\r", size);
 }
 
 /*
@@ -156,16 +199,73 @@ void setLeds()
 int main()
 {
     init_platform();
-    print("\r\nName: Elhyanah Desir\n\r"); 
-    print("\r\nCalling configureCodec()...\n\r");  
+    print("\n\rName: Elhyanah Desir\n\r"); 
+    print("Calling configureCodec()...\n\r");  
     configureCodec(); 
+    print("Playing ~6kHz tone for 5 seconds...\n\r");
+    for(int i = 0; i < 200000; i++)
+    {   waveGenerator();    }
     
-    //u16 volume = 0x79;
+    print("Welcome to audio playback system.\n\r"); 
+    print("\tpress L to load a file.\n\r");
+    print("\tpress C to playback continously followed by any key to stop.\n\r");
+    print("\tpress S to play the sound once.\n\r");
+    print("\tpress B to play a piercing beep for 2 seconds.\n\r");
+
     while(1)
     {
-        //setLeds();   //for Debugging 
-        waveGenerator();  
-        //modifyVolume(&volume);
+        if(XUartPs_IsReceiveData(XPAR_XUARTPS_0_BASEADDR))  
+        {
+            char c = XUartPs_RecvByte(XPAR_XUARTPS_0_BASEADDR);
+            c = toupper(c);
+        
+            switch(c)
+            {
+                case 'L':
+                    print("Please load/send file through terminal client.\n\r");
+                    loadFile();                
+                    break;
+
+                case 'C':
+                    print("Playing sound continously. Press any key to stop.\n\r");
+                    int stopped = 0;
+                    int count = 0;
+                    while(stopped == 0)
+                    {   
+                        writeToFIFO(count);  
+                        count = (count + 1) % (int)sampleSize;    
+
+                        if(XUartPs_IsReceiveData(XPAR_XUARTPS_0_BASEADDR))  
+                        { 
+                            stopped = 1; 
+                            print("Sound stopped.\n\r");                            
+                            (void) XUartPs_RecvByte(XPAR_XUARTPS_0_BASEADDR); 
+                        }                    
+                    }
+                    break;
+                
+                case 'S':
+                    print("Playing sound once.\n\r");
+                    for(int i = 0; i < (int)sampleSize; i++)
+                    {   writeToFIFO(i);  }
+                    break;
+
+                case 'B':
+                    print("Playing beep ....");                    
+                    for(int i = 0; i < 50000; i++)
+                    {   waveGenerator();    }
+                    print("Done.\n\r");
+                    break;
+
+                case 'P':
+                    for(int i = 0; i < (int)sampleSize; i++)
+                    {   printf("%08x\n", sampleData[i]); }
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
     cleanup_platform();
